6ffafb109a6570d2943c94f94d104c82fb930021
Dustin Martinez <martinezdustin736@gmail.com>
2020-03-07 11:22:40 -0600

    heroku

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use("/api/users", users);
 //app.use(express.static(path.join(__dirname, 'client')));
 //}
 
-//if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
   // Express serve up index.html file if it doesn't recognize route
@@ -43,7 +43,7 @@ app.use("/api/users", users);
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
  });
-//}
+}
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
