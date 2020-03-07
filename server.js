42f64a58bb1cbfe66ecaf005297263bb497cd134
Dustin Martinez <martinezdustin736@gmail.com>
2020-03-07 11:01:15 -0600

    Heroku files

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, 'client')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client')));
+}
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
